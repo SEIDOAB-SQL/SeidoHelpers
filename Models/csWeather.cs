e3df43248620aabd714501061afc57929a43c769
Martin <martin_lenart@icloud.com>
2024-05-13 10:23:50 +0200

    initial commit

---
+++ Models/csWeather.cs
@@ -0,0 +1,9 @@
+namespace Models
+{
+    public class csWeather
+    {
+        public decimal Temp { get; set; }
+        public string Visibility { get; set; }
+        public override string ToString() => $"{Visibility} {Temp} degC";
+    }
+}
